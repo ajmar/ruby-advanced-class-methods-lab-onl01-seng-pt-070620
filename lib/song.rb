dd8a91f7423629b3a7b83ed98e6e10b3915b128a
Alexander Marusin <amarusin18@gmail.com>
2020-07-29 02:24:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,20 @@ class Song
     self.find_by_name(name) ||
     self.create_by_name(name)
   end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    rows = filename.split(" - ")
+    artist_name = rows[0]
+    name = rows[1].gsub(".mp3", "")
+    song = Song.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
+
+  
 end
